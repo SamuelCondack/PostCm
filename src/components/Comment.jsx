373b8ce254c816472a7307b05c50215d6cb8d02b
Samuel Condack <samuelcondackxz@gmail.com>
2024-08-26 20:34:42 -0300

    Add handleLikeComment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,42 +1,57 @@
-import {Trash, ThumbsUp }from "phosphor-react"
-import styles from './Comment.module.scss'
-import { Avatar } from "./Avatar"
+import { Trash, ThumbsUp } from "phosphor-react";
+import styles from "./Comment.module.scss";
+import { Avatar } from "./Avatar";
+import { useState } from "react";
 
-export function Comment({ content, onDeleteComment }){
+export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
 
-    function handleDeleteComment(){
-        console.log('deletado');
+  function handleDeleteComment() {
+    console.log("deletado");
 
-        onDeleteComment(content);
-    }
+    onDeleteComment(content);
+  }
 
-    return(
-        <div className={styles.comment}>
-            <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/104593245?v=4" alt="" />
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1);
+  }
 
-            <div className={styles.commentBox}>
-                <div className={styles.commentContent}>
-                    <header>
-                        <div className={styles.authorAndTime}>
-                            <strong>Samuel Condack</strong>
-                            <time title="22 de Agosto às 20:56" dateTime="2024-08-22 20:56:10">Cerca de 10 minutos atrás</time>
-                        </div>
+  return (
+    <div className={styles.comment}>
+      <Avatar
+        hasBorder={false}
+        src="https://avatars.githubusercontent.com/u/104593245?v=4"
+        alt=""
+      />
 
-                        <button onClick={handleDeleteComment} title="Deletar comentário">
-                            <Trash size={24}/>
-                        </button>
-                    </header>
-
-                    <p>{content}</p>
-                </div>
-
-                <footer className={styles.commentFooter}>
-                    <button>
-                        <ThumbsUp/>
-                        Aplaudir <span>- 20</span>
-                    </button>
-                </footer>
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Samuel Condack</strong>
+              <time
+                title="22 de Agosto às 20:56"
+                dateTime="2024-08-22 20:56:10"
+              >
+                Cerca de 10 minutos atrás
+              </time>
             </div>
+
+            <button onClick={handleDeleteComment} title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>{content}</p>
         </div>
-    )
+
+        <footer className={styles.commentFooter}>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>- {likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
 }
